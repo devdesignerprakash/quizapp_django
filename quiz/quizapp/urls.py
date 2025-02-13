40475dde0c08e6208d12e2ab51eeaf68712f1f5e
devdesignerprakash <devdesignerprakash@gmail.com>
2025-02-13 08:20:56 +0545

    category data fetched successfully

--- quiz/quizapp/urls.py
+++ quiz/quizapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name="home"),
+    path('category/',views.category_view, name="category")
 ]
--- quiz/quizapp/views.py
+++ quiz/quizapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import*
 
 def home_view(request):
     return render(request,'app/home.html')
 
 # Create your views here.
+
+def category_view(request):
+    categories = Category.objects.all()  
+    print(categories)
+    context = {
+       "categories": categories
+    }
+    return render(request, 'app/categories.html', context)
